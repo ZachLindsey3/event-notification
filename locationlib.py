5bdb151dfef4b40a679bd1b0a42022caa8435c92
Zach Lindsey <zachlindsy3@gmail.com>
2023-09-21 00:38:51 -0700

    print precipitation

--- locationlib.py
+++ locationlib.py
@@ -54,13 +54,14 @@ class User:
     
     def print_info(self):
         # pp.pprint(self.gridpoint['properties']['forecast'])
-        # pp.pprint(self.forecast)
+        pp.pprint(self.forecast)
         print("test")
 
     def check_occurrence(self):
         for period in self.forecast['properties']['periods']:
             print(period['name'])
             print(period['temperature'])
+            print(period['probabilityOfPrecipitation']['value'])
 
 def main():
     # forcast_out = requests.get("http://dev.virtualearth.net/REST/v1/Locations?&postalCode={92109}&key={}")
@@ -74,7 +75,7 @@ def main():
     test_user.get_coords(api_key=location_api_key)
     test_user.get_gridpoint_url()
     test_user.get_forecast()
-    # test_user.print_info()
+    test_user.print_info()
     test_user.check_occurrence()
     print("+++++++++++")
     print("located")
